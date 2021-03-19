285019bad6b96538ff76063be98b3d9af37ea5db
spartanhero93 <spartanhero93@gmail.com>
2021-03-18 23:59:02 -0400

    removed redundant file/sidebar component added

---
+++ src/Components/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 25vw;
+  background-color: #282c34;
+`
+
+function SideBar() {
+  return(
+    <Container>
+
+    </Container>
+  )
+}
+
+export default SideBar
