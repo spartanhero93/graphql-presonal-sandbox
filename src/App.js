9a1c68b2a83241ea23fb591b73d52233afcb3cb7
spartanhero93 <spartanhero93@gmail.com>
2021-03-18 23:55:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { ApolloProvider } from '@apollo/client';
+import Header from './Containers/Header'
+
+
 import './App.css';
+import client from './Apollo';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="">
+        <Header />
+      </div>
+    </ApolloProvider>
   );
 }
 
